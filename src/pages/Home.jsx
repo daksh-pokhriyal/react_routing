f183519a87650b90236d90a8f7d24c9581ddb123
dakshpokhriyal <itisdakshpokhriyal@gmail.com>
2026-01-28 23:42:42 +0530

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1 className="home h1">Home Page</h1>
+      <p className="home p">This is the home page.</p>
+    </div>
+  );
+};
+
+export default Home;
